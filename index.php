f811e5c9a7f08f1ebd69b948d9ef35bd00c36e04
Abhishek Maheshwari <abhishek69744maheshwari@gmail.com>
2020-06-07 09:26:06 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<?php
+  require("includes/common.php");
+
+if (isset($_SESSION['email'])) {
+    header('location: products.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home | E-Store.com</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap Core CSS -->
+          <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+          <link href="css/main.css" rel="stylesheet">
+        <!-- jQuery -->
+          <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+          <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body style="padding-top: 50px;">
+
+       
+        <?php
+        include 'includes/header.php';
+        ?>
+        
+        <div id="content">
+            <!--Main banner image-->
+            <div id = "banner_image">
+                <div class="container">
+                    <center>
+                        <div id="banner_content">
+                            <h1>Biggest Online Mobile Store</h1>
+                            <h4><p>Flat &nbsp;&#8377;2,000&nbsp;&nbsp;OFF&nbsp;&nbsp;on all products.</p></h4>
+                            <br>
+                            <br>
+                            <a  href="products.php" class="btn btn-danger btn-lg active">Shop Now</a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <br>
+            <br>
+
+           <!--  Items -->
+            <div class="container text-center">
+                <div id="item_list">
+                    <div class="panel panel-default">
+                        <div class="panel-heading  color">
+                            <h3 style="color:orange">Choose&nbsp;&nbsp;From&nbsp;&nbsp;wide&nbsp;&nbsp;ranges&nbsp;&nbsp;of&nbsp;&nbsp;brands</h3>
+                        </div>
+
+                    <div class="panel-body">
+                        <div class="col-sm-3">
+                            <a href="products.php#ios">
+                               <div class="thumbnail">
+                                    <img src="images/5.jpg" alt="iphone x" >
+                                        <div class="caption">
+                                            <h3>Camera</h3>
+                                        </div>
+                                </div>
+                            </a>
+                        </div>
+
+                         <div class="col-sm-3">
+                            <a href="products.php#samsung">
+                               <div class="thumbnail">
+                                    <img src="images/5.jpg" alt="galaxy note 8 plus" >
+                                        <div class="caption">
+                                            <h3>Camera</h3>
+                                        </div>
+                                </div>
+                            </a>
+                        </div>
+
+                         <div class="col-sm-3">
+                            <a href="products.php#oneplus">
+                               <div class="thumbnail">
+                                    <img src="images/6.jpg" alt="oneplus" >
+                                        <div class="caption">
+                                            <h3>Shirt</h3>
+                                        </div>
+                                </div>
+                            </a>
+                        </div>
+
+                         <div class="col-sm-3">
+                            <a href="products.php#xiaomi">
+                               <div class="thumbnail">
+                                    <img src="images/6.jpg" alt="mi mix 2" >
+                                        <div class="caption">
+                                            <h3>Branded Shirt</h3>
+                                        </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                </div>
+            </div>
+
+        </div>
+
+        
+        <!--Footer-->
+        <?php
+        include 'includes/footer.php';
+        ?>
+
+    </body>
+</html>
